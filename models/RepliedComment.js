a620bb3455719fbe2ee0204af4764880ac5d1f87
bhaskarsriramp <bhaskarsriram.p@gmail.com>
2025-10-21 23:58:35 +0530

    Date 21 Oct v1.0

--- models/RepliedComment.js
+++ models/RepliedComment.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const RepliedCommentSchema = new Schema(
   {
     commentId: { type: String, required: true, unique: true, index: true },
-    automationId: { type: Schema.Types.ObjectId, ref: "Automation", required: true },
+    automationId: { type: Schema.Types.ObjectId, ref: "automations", required: true },
     repliedAt: { type: Date, default: Date.now },
     text: { type: String },
   },
