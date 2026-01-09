ea0e7f089c7dade740bec5412b8a80cba2f03b4a
Jobswitchco <jobswitchco@gmail.com>
2026-01-09 12:01:49 +0530

    Date 09 Jan v1.0

--- index.js
+++ index.js
@@ -1527,7 +1527,7 @@ app.post("/pubsub-messaging", async (req, res) => {
 
     for (const entry of entries) {
       console.log('entry : ', entry);
-      console.log('Recipient : ', entry?.messaging?.recipient);
+      console.log('Recipient : ', entry?.messaging[0].recipient);
       // ✅ EXTRACT BUSINESS ID (Page ID)
       // This is crucial for DM triggers to know WHICH business received the message
       const businessId = entry.id;
